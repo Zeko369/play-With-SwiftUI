85f6327a8318706f2a81c131a0984a82c035ef82
Fran Zekan <zekan.fran369@gmail.com>
2023-06-29 23:35:15 +0200

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Fran Zekan on 29.06.2023..
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
