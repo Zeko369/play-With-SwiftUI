38d0d3fd9725606995167fb570d022f72a29af73
Fran Zekan <zekan.fran369@gmail.com>
2023-06-29 23:54:46 +0200

    Add demo code

--- DemoApp/ContentView.swift
+++ DemoApp/ContentView.swift
@@ -8,19 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var show = true
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button("Open sheet") {
+            show.toggle()
         }
-        .padding()
+        .bold()
+        .sheet(isPresented: $show) {
+            VStack(alignment: .leading, spacing: 8) {
+                HStack {
+                    Spacer()
+                    Button("Done") {
+                        show = false
+                    }.bold()
+                }.padding(10)
+                
+                Text("Modal Sheet in SwiftUI").font(.largeTitle).bold()
+
+                Text("This is a dialog with a custom modal overlay built with SwiftUI")
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet nisl blandit, pellentesque eros eu, scelerisque eros. Sed cursus urna at nunc lacinia dapibus.")
+
+                Spacer()
+            }.padding(12)
+            
+            .presentationDetents([.large])
+            .presentationDragIndicator(.visible)
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
